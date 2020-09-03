9503d00fba6614a8fde552a384419fc19f053288
Magnus Frennberg <mfrennberg@hotmail.com>
2020-09-03 16:36:47 -0400

    Added TOAST on incorrect link

--- src/LinkInput.jsx
+++ src/LinkInput.jsx
@@ -1,8 +1,21 @@
 import React, { Component } from 'react';
 import './LinkInput.css';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSearch } from '@fortawesome/free-solid-svg-icons';
+toast.configure();
+
+function validateInput(inputValue) {
+	// Monstrosity from stackoverflow: https://stackoverflow.com/a/8234912
+	const regex = RegExp(
+		/((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=\+\$,\w]+@)?[A-Za-z0-9.-]+|(?:www.|[-;:&=\+\$,\w]+@)[A-Za-z0-9.-]+)((?:\/[\+~%\/.\w-_]*)?\??(?:[-\+=&;%@.\w_]*)#?(?:[\w]*))?)/,
+	);
+
+	const isValid = regex.test(inputValue);
+	return isValid;
+}
 
 export default class LinkInput extends Component {
 	constructor(props) {
@@ -14,21 +27,31 @@ export default class LinkInput extends Component {
 		// Submitting forms causes a page-reload -> Bad. Prevent it.
 		e.preventDefault();
 		// Here, we could validate the input.
-		// Then, we would have to get the next link.
-		// Right now, I'm just going to put it in the "history."
 		const { onSubmit } = this.props;
 		// This grabs the form DOM Element
 		const form = document.forms.link;
 		// This is the value of the link input
-        const submittedLink = form.linkInput.value;
-        form.linkInput.value = '';
-		onSubmit(submittedLink);
+		const submittedLink = form.linkInput.value;
+		const valid = validateInput(submittedLink);
+		if (!valid) {
+			// This should display an error.
+			toast.error('Incorrect Link', { position: toast.POSITION.BOTTOM_CENTER });
+		} else {
+			form.linkInput.value = '';
+			onSubmit(submittedLink);
+		}
 	}
 
 	render() {
 		return (
-			<form className="searchForm" name="link" onSubmit={(e) => this.handleSubmit(e)}>
-				<input type="text" name="linkInput" placeholder="Switcheroo Link"></input>
+			<form
+				className="searchForm"
+				name="link"
+				onSubmit={(e) => this.handleSubmit(e)}>
+				<input
+					type="text"
+					name="linkInput"
+					placeholder="Switcheroo Link"></input>
 				<button type="submit">
 					<FontAwesomeIcon icon={faSearch}></FontAwesomeIcon>
 				</button>
