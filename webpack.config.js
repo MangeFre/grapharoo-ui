18b5fff1ef04ed90dc61fd3207310cdc5f5f730d
Magnus Frennberg <mfrennberg@hotmail.com>
2020-09-04 12:57:57 -0400

    Changes to webpack & babel needed to use async/await

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,9 @@ module.exports = {
 				test: /\.(js|jsx)$/,
 				exclude: /(node_modules|bower_components)/,
 				loader: 'babel-loader',
-				options: { presets: ['@babel/env'] },
+				options: {
+					presets: ['@babel/env'],
+				},
 			},
 			{
 				test: /\.css$/,
@@ -31,4 +33,5 @@ module.exports = {
 		hotOnly: true,
 	},
 	plugins: [new webpack.HotModuleReplacementPlugin()],
+	devtool: 'source-map',
 };
