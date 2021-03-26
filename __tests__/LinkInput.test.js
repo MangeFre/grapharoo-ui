42fa79cfcb747453fbd6e688705c93ff35c21886
Magnus Frennberg <mfrennberg@hotmail.com>
2021-03-26 15:44:34 -0500

    Initial jest setup with snapshot

---
+++ __tests__/LinkInput.test.js
@@ -0,0 +1,12 @@
+// Link.react.test.js
+import React from 'react';
+import renderer from 'react-test-renderer';
+import LinkInput from '../src/LinkInput';
+
+const onSubmit = () => {};
+
+test('Create snapshot for future comparison', () => {
+    const component = renderer.create(<LinkInput onSubmit={onSubmit}></LinkInput>)
+    const tree = component.toJSON()
+    expect(tree).toMatchSnapshot();
+})
