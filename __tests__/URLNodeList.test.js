25403424f96b462a2d1f1b3fb3ad16e47e2166ff
jasc5797 <jasc5797@yahoo.com>
2021-04-08 02:25:14 -0400

    Updated GraphVisualizer.test.js to URLNodeList.test.js

--- __tests__/URLNodeList.test.js
+++ __tests__/URLNodeList.test.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
-import GraphVisualizer from '../src/GraphVisualizer';
+import URLNodeList from '../src/URLNodeList';
 
 const testLink = 'https://www.reddit.com/r/aww/comments/hd6xtp/comment/fvk5vao';
 
 test('Create snapshot for future comparison', () => {
-    const component = renderer.create(<GraphVisualizer origin={testLink}/>)
+    const component = renderer.create(<URLNodeList originUrl={testLink}/>)
     const tree = component.toJSON()
     expect(tree).toMatchSnapshot();
 })
